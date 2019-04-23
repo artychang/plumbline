c094d1c2420503c33db2b1ab98a8d092d9f35b48
Arthur Chang <nimblshop@gmail.com>
2019-04-23 13:58:37 -0700

    Add two-way binding for ngModel updating

--- src/utils/initComponent.ts
+++ src/utils/initComponent.ts
@@ -13,5 +13,13 @@ export function initComponent(template: any, bindings: any): any {
     @Component({ template: template })
     class RenderComponent {}
     Object.assign(RenderComponent.prototype, spyBindings(bindings));
+
+    // Enable two-way binding that overwrites & updates input
+    Object.keys(bindings).forEach((key) => {
+        Object.defineProperty(RenderComponent.prototype, key, {
+            get: () => bindings[key],
+            set: (v) => { bindings[key] = v; }
+        });
+    });
     return RenderComponent;
 }
